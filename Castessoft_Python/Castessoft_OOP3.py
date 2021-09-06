732b664b16e4c5b95298361fb197b2091dd38171
Juan Diego Castellanos Jerez <juan.castellanosj@usantoto.edu.co>
2021-09-06 14:08:39 -0500

    Primer Parte de Modulos

--- Castessoft_Python/Castessoft_OOP3.py
+++ Castessoft_Python/Castessoft_OOP3.py
@@ -1,5 +1,9 @@
 """
 Modulos
 """
-from Castessoft-OOP2 import Person
+from Castessoft_OOP2 import Person
+
+person0 = Person('Juan', 'Castellanos',21)
+print(f'Detalles: {person0.get_name} {person0.last_name} {person0.age}')
+
     
