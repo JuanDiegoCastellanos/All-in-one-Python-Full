ba28a3d129a060d5a83c1d99ff97cf5c9d3d2afa
Juan Diego Castellanos <juan.castellanosj@usantoto.edu.co>
2021-11-23 12:38:41 -0500

    filenames updated

--- Castessoft_Python/Castessoft_OOP3.py
+++ Castessoft_Python/Castessoft_OOP3.py
@@ -1,9 +1,17 @@
 """
 Modulos
 """
-from Castessoft_OOP2 import Person
+# from Castessoft_OOP2 import Person
 
-person0 = Person('Juan', 'Castellanos',21)
-print(f'Detalles: {person0.get_name} {person0.last_name} {person0.age}')
+# person0 = Person('Juan', 'Castellanos',21)
+# print(f'Detalles: {person0.get_name} {person0.last_name} {person0.age}')
 
-    
+diccionario = {'name':'Juan Diego Castellanos', 'password':'12345678'}
+
+print('name' in list(diccionario.keys()))
+print(diccionario['name']== 'Juan Da')
+
+
+
+
+
